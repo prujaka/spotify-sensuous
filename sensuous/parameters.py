7bd7b0b750e14dfcef7a4ae40289db920e7de1a1
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-09-04 20:46:10 +0200

    clean the naming mess

--- sensuous/parameters.py
+++ sensuous/parameters.py
@@ -6,7 +6,7 @@ LOCAL_CSV_PATH = os.environ.get("LOCAL_CSV_PATH")
 
 API_TYPE = os.environ.get("API_TYPE")
 API_URL_LOCAL = os.environ.get("API_URL_LOCAL")
-API_URL_CLOUD = os.environ.get("API_URL_GCR")
+API_URL_CLOUD = os.environ.get("API_URL_CLOUD")
 
 if __name__ == '__main__':
     print(f'LOCAL_MP3_DIR = {LOCAL_MP3_DIR}')
