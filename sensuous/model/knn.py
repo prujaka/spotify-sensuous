767aac50bd5fd033a76afa6dbdabe5e422aa70b4
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-09-04 15:53:37 +0200

    added the full csv data predict playlist function

--- sensuous/model/knn.py
+++ sensuous/model/knn.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import scipy as sp
+import os
 from sklearn.preprocessing import MinMaxScaler
 
 
@@ -8,16 +9,17 @@ def strip_artists(s):
     return s.replace("['", "").replace("']", "").replace("', '", ", ")
 
 
-def predict_playlist_lewagon(song='White Christmas',
-                             artist='Frank Sinatra',
-                             n_neighbors=10):
+def predict_playlist_csv(song_title: str = 'White Christmas',
+                         artist: str = 'Frank Sinatra',
+                         n_neighbors: int = 10,
+                         data_dir: str = 'data'):
     """Find `n_neighbors` closest neighbors of a given seed song in an audio
     feature space of le wagon spotify dataset. Only single artist is supported.
     If the song is not in the dataset, return the song's name.
 
     Parameters
     ----------
-    song : str
+    song_title : str
         Song's name.
 
     artist : str
@@ -26,29 +28,32 @@ def predict_playlist_lewagon(song='White Christmas',
     n_neighbors : int
         Number of neighbors to the seed song.
 
+    data_dir : str
+        CSV file directory
+
     Returns
     -------
     neighbors : list of shape (n_neighbors,) of tuples of shape (2,) of str
         List of tuples of the closest songs. First element of a tuple is
         a song name, the second one is the artist(s).
     """
-
-    # df = pd.read_csv('https://wagon-public-datasets.s3.amazonaws.com/'
-    #                  'Machine%20Learning%20Datasets/ML_spotify_data.csv')
-    df = pd.read_csv("data/lewagon-spotify-data.csv")
-    df = df.drop(['popularity'], axis=1)
-    df_numeric = df.drop(columns=['name', 'artists'])
+    df = pd.read_csv(os.path.join(data_dir, 'all-songs.csv'))
+    df_numeric = df.drop(columns=['artists', 'song_title'])
     columns = df_numeric.columns.values.tolist()
     df_scaler = MinMaxScaler()
     df[columns] = df_scaler.fit_transform(df[columns])
 
-    # Seed song selection
-    artists_formatted = f"['{artist}']"
-    seed_song = df[(df['name'] == song) & (df['artists'] == artists_formatted)]
+    criterion = ((df['artists'].map(lambda x: artist in x)
+                  | df['song_title'].map(lambda x: 'feat. ' + artist in x))
+                 & (df['song_title'].map(lambda x: song_title in x)))
+    seed_song = df[criterion]
 
     if seed_song.shape[0] == 0:
         print("No such a song in the dataset. Returning the user's entry.")
-        return f'{song} by {artist}'
+        return f'{song_title} by {artist}'
+    elif seed_song.shape[0] > 1:
+        print("There is more than one entry. Returning the search results.")
+        return seed_song
 
     # Feature "preprocessing"
     seed_song_features = seed_song.select_dtypes(exclude='object')
@@ -56,15 +61,16 @@ def predict_playlist_lewagon(song='White Christmas',
 
     # List of closest neighbors to the seed song
     tree = sp.spatial.KDTree(X.to_numpy())
-    distances, indeces = tree.query([seed_song_features], k=n_neighbors)
-    indeces = indeces.flatten()
-    neighbors_df = df.loc[list(indeces)]
-    songs = neighbors_df['name'].tolist()
+    distances, indices = tree.query([seed_song_features], k=n_neighbors)
+    indices = indices.flatten()
+    neighbors_df = df.loc[list(indices)]
+    songs = neighbors_df['song_title'].tolist()
     artists = [
         strip_artists(artist) for artist in neighbors_df['artists'].tolist()
     ]
+    playlist = {'playlist': list(zip(songs, artists))}
     return {'playlist': list(zip(songs, artists))}
 
 
 if __name__ == "__main__":
-    print(predict_playlist_lewagon())
+    print(predict_playlist_csv(data_dir="../../data"))
