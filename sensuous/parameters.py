6b2dfe579df28fd0a691beab4be326a3ae3a1d2c
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-09-04 20:30:37 +0200

    add local api urls

--- sensuous/parameters.py
+++ sensuous/parameters.py
@@ -3,5 +3,15 @@ import os
 LOCAL_MP3_DIR = os.environ.get("LOCAL_MP3_DIR")
 LOCAL_PNG_DIR = os.environ.get("LOCAL_PNG_DIR")
 LOCAL_CSV_PATH = os.environ.get("LOCAL_CSV_PATH")
-GCR_API_URL = os.environ.get("GCR_API_URL")
 
+API_TYPE = os.environ.get("API_TYPE")
+API_URL_LOCAL = os.environ.get("API_URL_LOCAL")
+API_URL_CLOUD = os.environ.get("API_URL_GCR")
+
+if __name__ == '__main__':
+    print(f'LOCAL_MP3_DIR = {LOCAL_MP3_DIR}')
+    print(f'LOCAL_PNG_DIR = {LOCAL_PNG_DIR}')
+    print(f'LOCAL_CSV_PATH = {LOCAL_CSV_PATH}')
+    print(f'API_TYPE = {API_TYPE}')
+    print(f'API_URL_LOCAL = {API_URL_LOCAL}')
+    print(f'API_URL_CLOUD = {API_URL_CLOUD}')
