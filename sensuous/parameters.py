6eee5f5eeb62b6872ecce2a01b6290bb54b48301
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-09-06 19:20:24 +0200

    removed gcp api urls and main function

--- sensuous/parameters.py
+++ sensuous/parameters.py
@@ -1,17 +1,9 @@
 import os
 
+API_TYPE = 'cloud'  # 'local' or 'cloud'
+
+API_URL_LOCAL = "http://0.0.0.0:8000"
+
 LOCAL_MP3_DIR = os.environ.get("LOCAL_MP3_DIR")
 LOCAL_PNG_DIR = os.environ.get("LOCAL_PNG_DIR")
 LOCAL_CSV_PATH = os.environ.get("LOCAL_CSV_PATH")
-
-API_TYPE = os.environ.get("API_TYPE")
-API_URL_LOCAL = os.environ.get("API_URL_LOCAL")
-API_URL_CLOUD = os.environ.get("API_URL_CLOUD")
-
-if __name__ == '__main__':
-    print(f'LOCAL_MP3_DIR = {LOCAL_MP3_DIR}')
-    print(f'LOCAL_PNG_DIR = {LOCAL_PNG_DIR}')
-    print(f'LOCAL_CSV_PATH = {LOCAL_CSV_PATH}')
-    print(f'API_TYPE = {API_TYPE}')
-    print(f'API_URL_LOCAL = {API_URL_LOCAL}')
-    print(f'API_URL_CLOUD = {API_URL_CLOUD}')
