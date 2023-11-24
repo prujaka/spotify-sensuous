32e10613552304b1f76f8505f7437dc90509f208
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-11-24 16:39:07 +0100

    refactor: remove unnecessary dummy method

--- sensuous/api/api_fast.py
+++ sensuous/api/api_fast.py
@@ -1,24 +1,12 @@
 from sensuous.model.knn import predict_playlist_csv
 from fastapi import FastAPI
 
-# TODO: output a link to docs and to test prediction?
-
 api = FastAPI()
 
-
-# root
 @api.get("/")
 def index():
     return {"status": "API connected"}
 
-
-# dummy predict endpoint
-@api.get("/dummy")
-def dummy(song):
-    return {'song recommendation': song}
-
-
-# first model for predicting the 10 nearest songs
 @api.get("/predict")
 def predict(artist: str, song: str):
     return predict_playlist_csv(artist, song)
