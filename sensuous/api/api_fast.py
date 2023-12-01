c31df8c5fec454b7afa7fe162fa9158075891a78
Sergey Tkachenko <sergeyprujina@gmail.com>
2023-12-01 12:25:46 +0100

    docs: add simple docstrings

--- sensuous/api/api_fast.py
+++ sensuous/api/api_fast.py
@@ -6,9 +6,11 @@ api = FastAPI()
 
 @api.get("/")
 def index():
+    """Displays welcome message when API is connected"""
     return {"status": "API connected"}
 
 
 @api.get("/predict")
 def predict(artist: str, song: str):
+    """Predicts the playlist based on the user's input"""
     return predict_playlist_csv(artist, song)
